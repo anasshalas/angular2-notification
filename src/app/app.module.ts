3280f3cfb278dcf22220fc80434bb91a3cdc3a95
anass <anass.elhalas@ebiznext>
2016-11-07 15:11:12 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {ToastModule, ToastOptions} from 'ng2-toastr/ng2-toastr';
 
-import { AppComponent } from './app.component';
-
+let options: any = {
+  animate: 'flyRight',
+  positionClass: 'toast-top-right',
+};
+    
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, 
+        ToastModule.forRoot(options),
+    ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+
+}    
